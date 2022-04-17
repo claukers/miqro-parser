d123babbf9f6ed76451d181c21d21f20ea58fa98
claukers <claukers@github>
2022-04-16 22:22:17 -0400

    more dict test

--- test/dict.test.js
+++ test/dict.test.js
@@ -1,4 +1,4 @@
-const {parse} = require("../dist");
+const {parse, Parser} = require("../dist");
 const {strictEqual} = require("assert");
 
 const testOptions = {
@@ -20,4 +20,27 @@ it("happy path", async () => {
   });
   strictEqual(parsed.dict.key, "value");
   strictEqual(parsed.dict.key2, "2");
-}, testOptions)
+}, testOptions);
+
+it("happy path 2", async () => {
+  const parser = new Parser();
+  const value = {
+    dict: {
+      key: "value",
+      key2: {
+        some: 1
+      }
+    }
+  }
+  parser.registerType("custom", {
+    some: "number"
+  });
+  const parsed = parser.parse(value, {
+    dict: {
+      type: "dict",
+      dictType: "string|custom"
+    }
+  });
+  strictEqual(parsed.dict.key, "value");
+  strictEqual(parsed.dict.key2.some, 1);
+}, testOptions);
